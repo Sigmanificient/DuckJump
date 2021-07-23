eb25d41f0fcb09e7ec216d65135c8351971c11f1
Sigmanificient <edhyjox@gmail.com>
2021-07-23 23:55:18 +0200

    Typing Scrolling Background

--- src/components/scrolling_background.py
+++ src/components/scrolling_background.py
@@ -1,20 +1,28 @@
+from typing import Optional
+
+import pygame
+
 from src.utils import load
 
-BACKGROUND_PATH = "src/assets/images/bg.png"
+BACKGROUND_PATH: str = "src/assets/images/bg.png"
 
 
 class ScrollingBackground:
-    _texture = None
+    """A Layer used to create a indefinitely scrolling background."""
+    _texture: Optional[pygame.Surface] = None
 
-    def __init__(self, offset):
-        self.rect = self.texture.get_rect()
+    def __init__(self, offset) -> None:
+        """Initialises infinite scroll background."""
+        self.rect: pygame.Rect = self.texture.get_rect()
         self.rect.x = offset
 
     @property
-    def texture(self):
+    def texture(self) -> pygame.Surface:
+        """Load the background texture."""
         if self._texture is None:
-            self._texture = load(BACKGROUND_PATH)
+            self._texture: pygame.Surface = load(BACKGROUND_PATH)
         return self._texture
 
-    def update(self):
+    def update(self) -> None:
+        """Move the scrolling background for a infinite effect."""
         self.rect.x = self.rect.x - 1 if self.rect.x > -1280 else 1280
